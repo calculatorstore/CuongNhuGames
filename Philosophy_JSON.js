fdbb6c6e9cc6d72f5788c4f8ba2a8a882650234b
CalculatorStore <CalculatorStore@gmail.com>
2025-01-06 22:42:13 -0800

    Create Philosophy_JSON.js

---
+++ Philosophy_JSON.js
@@ -0,0 +1,42 @@
+const philosophyData = [
+  {
+    Subject: "Self Defense",
+    Name: "5 A's of Self Defense",
+    List: [
+      "Awareness
+      "Alertness",
+      "Avoidance",
+      "Anticipation",
+      "Action"
+    ],
+    Explanation: "AWARENESS is key to self defense, without it the ability to defend ourselves is wasted, ALERTNESS is a heightened state of awareness. Do you know the difference between aware and alert? Being aware and alert will allow you to AVOID or ANTICIPATE any situation before it is too late and gives you the opportunity to use the proper ACTION. The student through daily dedicated practice can trust their instinct and act with the correct response."
+  },
+  {
+    Name: "Tenet #2",
+    Text: "All students should be faithful to the ideals of Cuong Nhu and attempt to spread and develop these beliefs within the younger generation so that they, too, may be morally and physically fit."
+  },
+  {
+    Name: "Tenet #3",
+    Text: "All members of Cuong Nhu are unified in spirit and respect each other and their instructors."
+  },
+  {
+    Name: "Tenet #4",
+    Text: "Only by absolute discipline of mind, body and spirit do students maintain honor in Cuong Nhu."
+  },
+  {
+    Name: "Tenet #5",
+    Text: "All members should respect all other styles of martial arts and only use martial arts techniques for self-defense and to protect truth and reason."
+  },
+  {
+    Name: "Tenet #6",
+    Text: "Cuong Nhu students, through dedicated daily practice, increase their spirit, stamina, and moral character."
+  },
+  {
+    Name: "Tenet #7",
+    Text: "The goal of Cuong Nhu students is to maintain a pure, simple, sincere, and noble life."
+  },
+  {
+    Name: "Tenet #8",
+    Text: "Self-confidence, self-control, modesty, and a non-defeatist attitude are the mental keynotes for students of Cuong Nhu."
+  }
+];
