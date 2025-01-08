0f97f099d042aa3bc06cc2b4a8955a8bf50c14fc
CalculatorStore <CalculatorStore@gmail.com>
2025-01-08 01:12:47 -0800

    Update Philosophy_JSON.js

--- Philosophy_JSON.js
+++ Philosophy_JSON.js
@@ -3,7 +3,7 @@ const philosophyData = [
     Subject: "Self Defense",
     Name: "5 A's of Self Defense",
     List: [
-      "Awareness
+      "Awareness",
       "Alertness",
       "Avoidance",
       "Anticipation",
@@ -12,31 +12,538 @@ const philosophyData = [
     Explanation: "AWARENESS is key to self defense, without it the ability to defend ourselves is wasted, ALERTNESS is a heightened state of awareness. Do you know the difference between aware and alert? Being aware and alert will allow you to AVOID or ANTICIPATE any situation before it is too late and gives you the opportunity to use the proper ACTION. The student through daily dedicated practice can trust their instinct and act with the correct response."
   },
   {
-    Name: "Tenet #2",
-    Text: "All students should be faithful to the ideals of Cuong Nhu and attempt to spread and develop these beliefs within the younger generation so that they, too, may be morally and physically fit."
+    Category: ""
+    Name: "Three-O’s Principle",
+    List: [
+      "Open mind",
+      "Open heart",
+      "Open arms"
+    ]
+    Explanation: ""
   },
   {
-    Name: "Tenet #3",
-    Text: "All members of Cuong Nhu are unified in spirit and respect each other and their instructors."
+    Category: ""
+    Name: "Five Firsts for Friendship",
+    List: [
+      "Communicate",
+      "Smile",
+      "Care",
+      "Share",
+      "Forgive"
+    ]
+    Explanation: ""
   },
   {
-    Name: "Tenet #4",
-    Text: "Only by absolute discipline of mind, body and spirit do students maintain honor in Cuong Nhu."
+    Category: ""
+    Name: "Five Steps to Health",
+    List: [
+      "Think",
+      "Eat",
+      "Exercise",
+      "Rest",
+      "Perform"
+    ]
+    Explanation: ""
   },
   {
-    Name: "Tenet #5",
-    Text: "All members should respect all other styles of martial arts and only use martial arts techniques for self-defense and to protect truth and reason."
+    Category: ""
+    Name: "Five S’s for Warmups",
+    List: [
+      "Safety",
+      "Slowness",
+      "Self-awareness,
+      "Stretching",
+      "Strengthening"
+    ]
+    Explanation: ""
   },
   {
-    Name: "Tenet #6",
-    Text: "Cuong Nhu students, through dedicated daily practice, increase their spirit, stamina, and moral character."
+    Category: ""
+    Name: "Five A’s for Self Defense",
+    List: [
+      "Awareness",
+      "Alertness",
+      "Avoidance",
+      "Anticipation",
+      "Action"
+    ]
+    Explanation: ""
   },
   {
-    Name: "Tenet #7",
-    Text: "The goal of Cuong Nhu students is to maintain a pure, simple, sincere, and noble life."
+    Category: ""
+    Name: "Five W’s for Self Defense",
+    List: [
+      "Wrong time",
+      "Wrong place",
+      "Wrong people",
+      "Wrong attitude",
+      "Wrong techniques"",
+    ]
+    Explanation: ""
   },
   {
-    Name: "Tenet #8",
-    Text: "Self-confidence, self-control, modesty, and a non-defeatist attitude are the mental keynotes for students of Cuong Nhu."
+    Category: ""
+    Name: "Five R’s for Self Defense",
+    List: [
+      "Right perspective",
+      "Right time",
+      "Right place",
+      "Right techniques",
+      "Run"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five P’s of Achievers",
+    List: [
+      "Perspective",
+      "Patience",
+      "Perseverance",
+      "Pride in accomplishment",
+      "Practice 'til perfect"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five D’s of a Winner",
+    List: [
+      "Direction",
+      "Discipline",
+      "Determination",
+      "Dedication",
+      "Do 'til death"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five Think Rights (No Brain, No Gain)",
+    List: [
+      "Think simpler",
+      "Think faster",
+      "Think better",
+      "Think deeper",
+      "Think wiser"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five Res’s for Interrelating",
+    List: [
+      "Responsibility (self respect)",
+      "Respect others",
+      "Responsiveness",
+      "Resolution",
+      "Resilience"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five F’s for Learning and Teaching",
+    List: [
+      "Fun",
+      "Friendship",
+      "Focus",
+      "Fly your spirit",
+      "Finish your goal"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five Re’s of Meditation",
+    List: [
+      "Relaxation",
+      "Respiration",
+      "Renewal / rejuvenation",
+      "Reflection",
+      "Realization"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five Sources of Power",
+    List: [
+      "Mind",
+      "Body",
+      "Spirit",
+      "Soul",
+      "Ki"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Seven Martial Arts of Influence",
+    List: [
+      "Shotokan",
+      "Boxing",
+      "Judo",
+      "Aikido",
+      "Wing Chun",
+      "Tai Chi Chuan",
+      "Vovinam"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five Controls for Self Defense",
+    List: [
+      "Control area (awareness)",
+      "Control yourself (fear, anger)",
+      "Control attackers (strategy)",
+      "Control the situation",
+      "Control consequences"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five All’s for Self Defense (3-D)",
+    List: [
+      "All angles",
+      "All levels",
+      "All techniques",
+      "All situations",
+      "All people"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five Wins of a Winner",
+    List: [
+      "Over yourself",
+      "Opponent’s respect",
+      "Third party’s respect",
+      "Over the situation",
+      "People’s hearts"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Ten Don’ts for Sparring",
+    List: [
+      "Angry",
+      "Fearful",
+      "Tense",
+      "Hurried",
+      "Waste energy",
+      "Overconfident",
+      "Distracted",
+      "Pre-conceived ideas",
+      "Discouraged if you lose",
+      "Afraid of losing"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five Steps in Cuong Nhu Philosophy",
+    List: [
+      "Living",
+      "Giving",
+      "Caring",
+      "Sharing",
+      "Loving"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five Selves",
+    List: [
+      "Self-acceptance / self-tolerance",
+      "Self-confidence",
+      "Self-esteem",
+      "Self-control",
+      "Selflessness"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Eight Sources of Learning",
+    List: [
+      "Sensei and sempai",
+      "Classmates",
+      "Mirrors",
+      "Books, media",
+      "Tournaments",
+      "Training equipment",
+      "Yourself",
+      "Seminars"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five Re’s for Healing",
+    List: [
+      "Reduce",
+      "Relax",
+      "Reach",
+      "Reevaluate",
+      "Remember"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "The Foundation for Peak Performance: Simple as ABCDE",
+    List: [
+      "Awareness (well prepared and planned) (right perspective, right attitude)",
+      "Belief (in self and others)",
+      "Commitment (to excellence)",
+      "Dedication (labor of love)",
+      "Enlightenment"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five F’s for Training",
+    List: [
+      "Fortify your body",
+      "Find your own way",
+      "Fly your spirit",
+      "Feed your soul",
+      "Flow your ki"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five S’s for Safe Training / Running",
+    List: [
+      "Sane – Sight",
+      "Stretching",
+      "Stamina",
+      "Strength",
+      "Speed"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Ten C’s for Successful Training / Running",
+    List: [
+      "Commitment",
+      "Coaching",
+      "Consistency",
+      "Courage",
+      "Conditioning",
+      "Camaraderie",
+      "Concentration",
+      "Communication",
+      "Competition with yourself",
+      "Control of your body"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Training / Running Relation to Other Sports",
+    List: [
+      "Focus (timing)",
+      "Power",
+      "Endurance",
+      "Flexibility (conditioning)",
+      "Perspective"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Ten “Nesses” of Leadership",
+    List: [
+      "Fitness – Health",
+      "Wellness – Health",
+      "Assertiveness – PR",
+      "Openness – PR",
+      "Fairness - PR",
+      "Directness – PR",
+      "Oneness – Improvement",
+      "Togetherness – Improvement",
+      "Forgiveness – Improvement",
+      "Creativeness – Improvement"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "5 Ups of Learning and Teaching",
+    List: [
+      "Shut Up",
+      "Put Up",
+      "Beat Up",
+      "Keep Up",
+      "Live Up"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five Troubles To Avoid",
+    List: [
+      "Alcohol abuse",
+      "Bigoted bull",
+      "Cardiovascular carelessness",
+      "Drug dependency",
+      "Excessive ego"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Ten Stages of Growth",
+    List: [
+      "Nobody",
+      "Learner",
+      "Worker",
+      "Fighter",
+      "Achiever",
+      "Winner",
+      "Teacher",
+      "Leader",
+      "Thinker",
+      "Philosopher",
+      "Nobody"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five Loves of a Sensei",
+    List: [
+      "Teaching",
+      "Sharing",
+      "Students",
+      "Martial arts",
+      "Growth"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five Negatives in Teaching",
+    List: [
+      "Don’t ridicule any student",
+      "Don’t make discriminatory remarks",
+      "Don’t push students excessively (to avoid burning them out)",
+      "Don’t show favoritism",
+      "Don’t lose your composure"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Twelve “Tions” for Teaching",
+    List: [
+      "Communication",
+      "Simplification",
+      "Explanation",
+      "Demonstration",
+      "Repetition",
+      "Correction",
+      "Creation",
+      "Interrelation",
+      "Reflection",
+      "Motivation",
+      "Evaluation",
+      "Dedication"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five Trues for Love of Life",
+    List: [
+      "True friendship",
+      "True love",
+      "True happiness",
+      "True peace",
+      "True freedom"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "5 Fears of Achievement",
+    List: [
+      "Hard work",
+      "Failure",
+      "Intimidation or pressure",
+      "Associating with people",
+      "Death"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five Togethers at Annual Training",
+    List: [
+      "Physically we train together",
+      "Socially we eat and live together",
+      "Intellectually we think together",
+      "Spiritually we learn, share and grow together",
+      "Enjoy togetherness and harmony"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "Five Saves in Wing Chun",
+    List: [
+      "Distance",
+      "Energy",
+      "Manpower",
+      "Time",
+      "Life"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "5 E's in Education",
+    List: [
+      "Expertise",
+      "Eagerness",
+      "Enthusiasm",
+      "Enhancement",
+      "Enamor"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "5 C's for Good Communication",
+    List: [
+      "Clear",
+      "Concise",
+      "Concrete",
+      "Consultative (2-way)",
+      "Composed"
+    ]
+    Explanation: ""
+  },
+  {
+    Category: ""
+    Name: "5 V 's in Poor Communication",
+    List: [
+      "Vague",
+      "Verbose",
+      "Variable",
+      "Vacant",
+      "Vituperative"
+    ]
+    Explanation: ""
   }
 ];
