1f44083912d86843da8317dd31b4ab2a365a697f
CalculatorStore <CalculatorStore@gmail.com>
2025-01-15 17:16:10 -0800

    Added order summary
    Added order summary

--- CodeOfEthics_JSON.js
+++ CodeOfEthics_JSON.js
@@ -5,41 +5,49 @@ const codeOfEthicsData = [
   {
     Name: "Tenet #1",
     Text: "Cuong Nhu students should strive to improve themselves and their abilities in the martial arts in order to serve the people.",
-    Explanation: "Your training should not be self-serving. If you only teach, but do not work out, you only feed the ego. If you strive to serve others, you can know yourself through them as you learn and grow. You grow if you continually flow out toward others. Grow with other students as well as your own to improve the quality of Cuong Nhu Martial Arts. Students and instructors should share pride in the development of the martial arts. Develop an open mind to new ideas for improving and serving humanity. Serving others allows you to forget yourself."
+    Explanation: "Your training should not be self-serving. If you only teach, but do not work out, you only feed the ego. If you strive to serve others, you can know yourself through them as you learn and grow. You grow if you continually flow out toward others. Grow with other students as well as your own to improve the quality of Cuong Nhu Martial Arts. Students and instructors should share pride in the development of the martial arts. Develop an open mind to new ideas for improving and serving humanity. Serving others allows you to forget yourself.",
+    Summary: "Care about self and others"
   },
   {
     Name: "Tenet #2",
     Text: "All students should be faithful to the ideals of Cuong Nhu and attempt to spread and develop these beliefs within the younger generation so that they, too, may be morally and physically fit.",
-    Explanation: "Develop completely the attitude of perseverance and patience. Never give up or quit. Development of the younger generation is like growth in nature. It takes three months to plant a garden and receive the fruits of your labor. Several years are needed to plant a tree and see the results. To develop the people into morally, physically and spiritually strong individuals takes one hundred years."
+    Explanation: "Develop completely the attitude of perseverance and patience. Never give up or quit. Development of the younger generation is like growth in nature. It takes three months to plant a garden and receive the fruits of your labor. Several years are needed to plant a tree and see the results. To develop the people into morally, physically and spiritually strong individuals takes one hundred years.",
+    Summary: "Faith and sharing"
   },
   {
     Name: "Tenet #3",
     Text: "All members of Cuong Nhu are unified in spirit and respect each other and their instructors.",
-    Explanation: "Each of you is an individual with a distinct personality and role in life. You can learn by yourself, but if you grow with others in mutual respect, you develop harmony. From an individual musician, we hear a lone instrument. But join it with the efforts of many musicians, and the physical and spiritual strengths of each combine to produce beautiful music."
+    Explanation: "Each of you is an individual with a distinct personality and role in life. You can learn by yourself, but if you grow with others in mutual respect, you develop harmony. From an individual musician, we hear a lone instrument. But join it with the efforts of many musicians, and the physical and spiritual strengths of each combine to produce beautiful music.",
+    Summary: "Oneness and togetherness"
   },
   {
     Name: "Tenet #4",
     Text: "Only by absolute discipline of mind, body and spirit do students maintain honor in Cuong Nhu.",
-    Explanation: "With discipline in all three — body, mind and spirit — you can discover the Way. Determination and discipline is your mental path. A healthy body and hard training is your physical path. Courage, love and caring is your spiritual path. As you travel those paths, strive always to seek the best. Create for yourself a love for perfection and you will gain honor through honesty, loyalty, sincerity and pride. Yours will be not a possessive love, but an unconditional love."
+    Explanation: "With discipline in all three — body, mind and spirit — you can discover the Way. Determination and discipline is your mental path. A healthy body and hard training is your physical path. Courage, love and caring is your spiritual path. As you travel those paths, strive always to seek the best. Create for yourself a love for perfection and you will gain honor through honesty, loyalty, sincerity and pride. Yours will be not a possessive love, but an unconditional love.",
+    Summary: "Discipline and honor"
   },
   {
     Name: "Tenet #5",
     Text: "All members should respect all other styles of martial arts and only use martial arts techniques for self-defense and to protect truth and reason.",
-    Explanation: "Have harmony with others in the martial arts. If you like some styles but reject others, you cannot keep a balance in your attitude toward, and application of, the code of ethics."
+    Explanation: "Have harmony with others in the martial arts. If you like some styles but reject others, you cannot keep a balance in your attitude toward, and application of, the code of ethics.",
+    Summary: "Responsibility and purpose"
   },
   {
     Name: "Tenet #6",
     Text: "Cuong Nhu students, through dedicated daily practice, increase their spirit, stamina, and moral character.",
-    Explanation: "If you stop working out, you will decline. Ceasing to grow is the first sign of dying. Daily training gives you good health, balances your mind and body, and above all, brings you a positive and loving attitude toward life."
+    Explanation: "If you stop working out, you will decline. Ceasing to grow is the first sign of dying. Daily training gives you good health, balances your mind and body, and above all, brings you a positive and loving attitude toward life.",
+    Summary: "Mind, body and spirit building"
   },
   {
     Name: "Tenet #7",
     Text: "The goal of Cuong Nhu students is to maintain a pure, simple, sincere, and noble life.",
-    Explanation: "Early in life you closely identified with these values. As you grew, the struggle to survive the pitfalls of society and bad experiences in life conspired to weaken those ideals. You must now develop a healthy resistance to cor- rupting influences so you can return to nature. Embrace a simple life by accepting and doing your best. Do not expect too much from others. If they fall short, accept their shortcomings with understanding. Strive for sincerity and nobility in your actions. Simplicity and purity make life meaningful and worthwhile."
+    Explanation: "Early in life you closely identified with these values. As you grew, the struggle to survive the pitfalls of society and bad experiences in life conspired to weaken those ideals. You must now develop a healthy resistance to cor- rupting influences so you can return to nature. Embrace a simple life by accepting and doing your best. Do not expect too much from others. If they fall short, accept their shortcomings with understanding. Strive for sincerity and nobility in your actions. Simplicity and purity make life meaningful and worthwhile.",
+    Summary: "Soul crystallization"
   },
   {
     Name: "Tenet #8",
     Text: "Self-confidence, self-control, modesty, and a non-defeatist attitude are the mental keynotes for students of Cuong Nhu.",
-    Explanation: "As you develop a balance within yourself and harmony with others, you build values that make you self- confident. Self-control means maintaining your emotions. If you break this balance in yourself, it can carry over in your attitude toward others. If you are modest, you become more aware and are always ready tc learn. Others will sense this, and you will gain their respect. Arrogance, on the other hand, is a weakness because it stunts your learning ability. Having a non- defeatist attitude takes courage and sacrifice. From this you overcome yourself and constantly grow through your difficulties and hardships."
+    Explanation: "As you develop a balance within yourself and harmony with others, you build values that make you self- confident. Self-control means maintaining your emotions. If you break this balance in yourself, it can carry over in your attitude toward others. If you are modest, you become more aware and are always ready tc learn. Others will sense this, and you will gain their respect. Arrogance, on the other hand, is a weakness because it stunts your learning ability. Having a non- defeatist attitude takes courage and sacrifice. From this you overcome yourself and constantly grow through your difficulties and hardships.",
+    Summary: "Balance and resiliency"
   }
 ];
